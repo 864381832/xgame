e1f022221404db7fc80140ac044cbfdcdf6c6c7a
fxu <fxu@easipass.com>
2025-05-05 18:30:43 +0800

    添加游戏

---
+++ DangTiaoWang/src/settings.6ba37.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/LoadingScene.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"4f657",main:"c4911"}};
